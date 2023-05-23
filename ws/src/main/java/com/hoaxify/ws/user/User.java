402a0cfe714bf94ff7402510eb373612355905f3
Mehmet Fevzi Akdeniz <m.fevziakdeniz@gmail.com>
2023-05-23 22:22:30 +0300

    bean validation yapildi

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data //lombok kutuphanesi sayesinde tanimlanan degiskenlere ait getter setter fonksiyonlarini bizim yerimizie tanimlanmis olur
@@ -13,7 +14,9 @@ public class User {
 	@Id
 	@GeneratedValue //default hali otomatik olarak id degeri uretir
 	private long id;
+	@NotNull
 	private String userName;
+	@NotNull
 	private String displayName;
 	private String password;
 }
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class UserController {
 	
@@ -28,17 +30,28 @@ public class UserController {
 	//react projemizdeki 'package.json' dosyasinda yaptigimiz proxy ayari sayesinde crossorigin'a ihtiyacimiz kalmadi
 	//@CrossOrigin //react'in calistigi port ile spring boot'un calistigi port farkli oldugunda bu farkliliga takilmadan isler yurusun diye
 	@PostMapping("/api/users")
-	public /*GenericResponse*/ResponseEntity<?> createUser(@RequestBody User user) { //string turundeki json verisini benim istedigim nesne turunde ver diyoruz. Bunu 'Jackson' kutuphanesi sayesinde yapiyor
+	//@Valid ile, gelen requestin entity bazinda valid olmasi gerektigini soyluyoruz. Orada tanimlanan sekle uygun olmali
+	public /*GenericResponse*/ResponseEntity<?> createUser(@Valid @RequestBody User user) { //string turundeki json verisini benim istedigim nesne turunde ver diyoruz. Bunu 'Jackson' kutuphanesi sayesinde yapiyor
+		ApiError err = new ApiError(400, "validation error", "/api/users");
+		Map<String, String> validationErrors = new HashMap<>();
+		
 		String username = user.getUserName();
-		if(username == null || username.isEmpty()) {
-			ApiError err = new ApiError(400, "validation error", "/api/users");
-			Map<String, String> validationErrors = new HashMap<>();
+		String displayName = user.getDisplayName();
+		
+		if(username == null || username.isEmpty()) {	
 			validationErrors.put("kullaniciAdi", "kullanici adi bos birakilamaz!");
+		}
+		if(displayName == null || displayName.isEmpty()) {	
+			validationErrors.put("displayName", "displayName bos birakilamaz!");
+		}
+		
+		if(validationErrors.size() > 0) {
 			err.setValidationErrors(validationErrors);
 			//validation hatalarinda bad request donmek gerekir 400 hatasi
 			//responseEntity generic bir class'tir ve ne dondugunu belirtmek gerekir
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 		}
+		
 		userService.save(user);
 		log.info(user.toString());
 		return ResponseEntity.ok(new GenericResponse("User created")); 
