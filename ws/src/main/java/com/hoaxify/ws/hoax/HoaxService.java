36b716b6c0e7fc04518eb90cecba7b8814a1fdd8
Mehmet Fevzi Akdeniz <m.fevziakdeniz@gmail.com>
2023-08-04 17:44:39 +0300

    bir kullaniciya ait hoax'larin listelenmesi

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxService.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 
 import com.hoaxify.ws.user.User;
+import com.hoaxify.ws.user.UserRepository;
+import com.hoaxify.ws.user.UserService;
 
 import jakarta.validation.Valid;
 
@@ -14,9 +18,11 @@ import jakarta.validation.Valid;
 public class HoaxService {
 	
 	HoaxRepository hoaxRepository;
+	UserService userService;
 
-	public HoaxService(HoaxRepository hoaxRepository) {
+	public HoaxService(HoaxRepository hoaxRepository, UserService userService) {
 		this.hoaxRepository = hoaxRepository;
+		this.userService = userService;
 	}
 
 	public void save(Hoax hoax, User user) {
@@ -27,6 +33,12 @@ public class HoaxService {
 
 	public Page<Hoax> getHoaxes(Pageable page) {
 		return hoaxRepository.findAll(page);
+	}
+
+	public Page<Hoax> getHoaxesOfUser(String username, Pageable page) {
+		//username bilgisine sahip kullanicinin varligini kontrol edelim
+		User inDb = userService.getByUsername(username);
+		return hoaxRepository.findByUser(inDb, page);
 	}
 	
 	
